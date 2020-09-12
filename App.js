ffeb265d1cd910edc930bc679a003a1e9ef731b1
Ananya Vaidya <akv4@rice.edu>
2020-09-12 04:09:20 -0500

    added text to speech

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "./screens/HomeScreen";
 import Connecting from "./screens/Connecting";
-import Bot from "./screens/Bot";
+import BotMultiline from "./screens/Bot";
 import Chat from "./screens/Chat";
 
 const Stack = createStackNavigator();
@@ -19,7 +19,7 @@ class App extends React.Component {
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="Connecting" component={Connecting} />
           <Stack.Screen name="Chat" component={Chat} />
-          <Stack.Screen name="Bot" component={Bot} />
+          <Stack.Screen name="Bot" component={BotMultiline} />
         </Stack.Navigator>
       </NavigationContainer>
     );
--- screens/Bot.js
+++ screens/Bot.js
@@ -23,14 +23,60 @@ import React from 'react'
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
 import * as Speech from 'expo-speech'
 
-const Bot = () => (
-    <Button title="Speak!" onPress={() => Speech.speak('Hello World!')} />
-    <Button
-        title="Back to home"
-        onPress={() =>
-            this.props.navigation.navigate('Home')
-        }
-    />
-);
+// const Bot = () => (
+//     <Button title="Speak!" onPress={() => Speech.speak('Hello World!')} />
+//     <Button
+//         title="Back to home"
+//         onPress={() =>
+//             this.props.navigation.navigate('Home')
+//         }
+//     />
+// );
 
-export default Bot;
+// export default Bot; 
+
+const Bot = (props) => {
+    return (
+        <TextInput
+            {...props} // Inherit any props passed to it; e.g., multiline, numberOfLines below
+            editable
+            maxLength={40}
+        />
+    );
+}
+
+const BotMultiline = () => {
+    const [value, onChangeText] = React.useState('Useless Multiline Placeholder');
+
+    // If you type something in the text box that is a color, the background will change to that
+    // color.
+    return (
+        <div>
+            <View
+                style={{
+                    backgroundColor: value,
+                    borderBottomColor: '#000000',
+                    borderBottomWidth: 1,
+                }}>
+                <Bot
+                    multiline
+                    numberOfLines={4}
+                    onChangeText={text => onChangeText(text)}
+                    value={value}
+                />
+            </View>
+
+            <Button title="Speak!" onPress={() => Speech.speak(value)} />
+            {console.log("onCT", onChangeText)}
+            {console.log(value)}
+            <Button
+                title="Back to home"
+                onPress={() =>
+                    this.props.navigation.navigate('Home')
+                }
+            />
+        </div>
+    );
+}
+
+export default BotMultiline;
