01e918cebd9bae035fe68dd1685391eaa42e1a5f
Ananya Vaidya <akv4@rice.edu>
2020-09-12 01:21:56 -0500

    added phonecall to chat

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,19 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, SafeAreaView } from 'react-native';
+
+import { Linking } from "react-native";
+// import { SafeAreaView, Text, Button, TouchableOpacity } from "react-native";
+import Communications from "react-native-communications";
 
 class Chat extends React.Component {
     render() {
         return (
-            <View>
-                <Text>Chat</Text>
+            // <View>
+            //     <Text>Chat</Text> 
 
+            //     <Button
+            //         title="Back to home"
+            //         onPress={() =>
+            //             this.props.navigation.navigate('Home')
+            //         }
+            //     />
+            // </View>
+            <SafeAreaView>
+                <TouchableOpacity
+                    onPress={() => Communications.phonecall("4088968867", true)}
+                >
+                    <Text>Make phonecall</Text>
+                </TouchableOpacity>
                 <Button
                     title="Back to home"
                     onPress={() =>
                         this.props.navigation.navigate('Home')
                     }
                 />
-            </View>
+            </SafeAreaView>
         );
     }
 }
