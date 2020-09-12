3bf1c776a6fd0a626926f5288bd4a5a8ecabe803
Ananya Vaidya <akv4@rice.edu>
2020-09-12 02:38:24 -0500

    test config

--- firebase/config.js
+++ firebase/config.js
@@ -4,7 +4,6 @@ import "@firebase/firestore";
 // import { proc } from 'react-native-reanimated';
 
 const firebaseConfig = {
-<<<<<<< HEAD
     apiKey: process.env.apiKey,
     authDomain: process.env.authDomain,
     databaseURL: "https://pennapps2020-eac03.firebaseio.com",
@@ -14,20 +13,10 @@ const firebaseConfig = {
     messagingSenderId: process.env.messagingSenderId,
     appId: process.env.appId,
     measurementId: process.env.measurementId,
-=======
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  databaseURL: process.env.databaseURL,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId,
->>>>>>> 5a005e2599e5aa39c3188c1063e42d2bc35f5299
 };
 
 if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
+    firebase.initializeApp(firebaseConfig);
 }
 
 export { firebase };
