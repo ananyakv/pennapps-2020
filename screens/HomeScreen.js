8e43c6edc16ed175394b1f24faca740019d6caaa
Adrienne <adriennehl36@gmail.com>
2020-09-12 02:38:52 -0500

    id of entry is current date

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,14 +6,24 @@ import { firebase } from '../firebase/config'
 
 function HomeScreen(props) {
     
+    const getDate = () => {
+        var date = new Date().getDate(); //To get the Current Date
+        var month = new Date().getMonth() + 1; //To get the Current Month
+        var year = new Date().getFullYear(); //To get the Current Year
+        var hours = new Date().getHours(); //To get the Current Hours
+        var min = new Date().getMinutes(); //To get the Current Minutes
+        var sec = new Date().getSeconds(); //To get the Current Seconds
+        return new Date(year, month, date, hours, min, sec);
+    }
+    
     const joinQueue = () => { 
         firebase.database().ref('queue/count').once("value", snapshot => {
             if (snapshot.exists()) {
                 const position = snapshot.val() + 1
-                firebase.database().ref('queue/' + position).set({
-                    phone: "243289"
+                const date = getDate();
+                firebase.database().ref('queue/' + date).set({
+                    phone: 4088968867
                   })
-                  console.log(position)
                   firebase.database().ref('queue/count').set(position)
                   Alert.alert(
                     "Joined Queue!",
