a88e131d5d7fa5d94984a225f02741b31a917d8d
Ananya Vaidya <akv4@rice.edu>
2020-09-12 03:49:21 -0500

    added hello world speech to text

--- screens/Bot.js
+++ screens/Bot.js
@@ -1,23 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+// import React from 'react';
+// import { StyleSheet, Text, View, Button } from 'react-native';
 
-class Bot extends React.Component {
-    render() {
-        return (
-            <View>
-                <Text>Bot</Text>
+// import Tts from 'react-native-tts';
 
-                <Button
-                    title="Back to home"
-                    onPress={() =>
-                        this.props.navigation.navigate('Home')
-                    }
-                />
-            </View>
-        );
-    }
-}
+// class Bot extends React.Component {
+//     render() {
+//         return (
+//             <View>
+//                 <Text>Bot</Text>
+//                 <Button
+//                     title="Back to home"
+//                     onPress={() =>
+//                         this.props.navigation.navigate('Home')
+//                     }
+//                 />
+//             </View>
+//         );
+//     }
+// }
 
-// ...
+import React from 'react'
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
+import * as Speech from 'expo-speech'
+
+const Bot = () => (
+    <Button title="Speak!" onPress={() => Speech.speak('Hello World!')} />
+    <Button
+        title="Back to home"
+        onPress={() =>
+            this.props.navigation.navigate('Home')
+        }
+    />
+);
 
 export default Bot;
