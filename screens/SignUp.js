bace138440cd8677ca1c2dce27031761be5ea8e0
Tiffany Xiang <tiffanykelli16@gmail.com>
2020-09-13 03:16:59 -0500

    merging

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -75,19 +75,5 @@ function SignUp(props) {
     </TouchableWithoutFeedback>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  textInput: {
-    height: 40,
-    width: "90%",
-    borderColor: "gray",
-    borderWidth: 1,
-    marginTop: 8,
-  },
-});
 
 export default SignUp;
