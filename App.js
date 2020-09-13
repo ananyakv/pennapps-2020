fdef5056c72ac6f0da382b971856041295d897e9
azhangyy <55417685+azhangyy@users.noreply.github.com>
2020-09-12 23:21:52 -0500

    Log In, Sign Up Functionality

--- App.js
+++ App.js
@@ -9,88 +9,85 @@ import Connecting from "./screens/Connecting";
 import BotMultiline from "./screens/Bot";
 import Chat from "./screens/Chat";
 import LogInScreen from "./screens/LogInScreen";
-import PhoneCall from "/Users/angela/Documents/GitHub/pennapps-2020/PhoneCall.js";
+import SignUp from "./screens/SignUp";
+import LandingScreen from "./screens/LandingScreen";
+import PhoneCall from "./PhoneCall.js";
 import Styles from "./Styles";
 
 import * as firebase from "firebase";
-// import auth from "@react-native-firebase/auth";
+import * as firebaseui from "firebaseui";
+import auth from "@react-native-firebase/auth";
+
+const Stack = createStackNavigator();
+
+// const handleLogIn = () => {
+//   console.log("hi", user);
+//   firebase
+//     .auth()
+//     .signInWithPhoneNumber(phoneNum)
+//     .catch((err) => {
+//       switch (err.code) {
+//         case "auth/invalid-phoneNum":
+//           setPhoneError(err.message);
+//           break;
+//       }
+//     });
+// };
 
 function App() {
-  const [user, setUser] = useState("");
-  const [phoneNum, setPhoneNum] = useState("");
-  const [phoneError, setPhoneError] = useState("");
-
-  const Stack = createStackNavigator();
-
-  const handleLogIn = () => {
-    console.log("hi", user);
-    firebase
-      .auth()
-      .signUpWithPhoneNumber(phoneNum)
-      .catch((err) => {
-        switch (err.code) {
-          case "auth/invalid-phoneNum":
-            setPhoneError(err.message);
-            break;
-        }
-      });
-  };
-  const authListener = () => {
-    firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        setUser(user);
-      } else {
-        setUser("");
-      }
-    });
-  };
-
+  const [isLogged, setIsLogged] = useState();
   useEffect(() => {
-    authListener();
-  }, []);
+    firebase.auth().onAuthStateChanged((user) => {
+      setIsLogged(user);
+    });
+  });
 
-  return (
-    <View>
-      {user ? (
-        <HomeScreen />
-      ) : (
-        <LogInScreen
-          phoneNum={phoneNum}
-          setPhoneNum={setPhoneNum}
-          handleLogIn={handleLogIn}
-          phoneError={phoneError}
-        />
-      )}
-    </View>
-  );
-
-  // return (
-  //   <NavigationContainer>
-  //     <Stack.Navigator /*screenOptions={{ headerShown: false }}*/>
-  //       <Stack.Screen
-  //         name="Home"
-  //         component={HomeScreen}
-  //         options={{ header: () => null }}
-  //       />
-  //       <Stack.Screen
-  //         name="Connecting"
-  //         component={Connecting}
-  //         options={{ header: () => null }}
-  //       />
-  //       <Stack.Screen
-  //         name="Chat"
-  //         component={Chat}
-  //         options={{ header: () => null }}
-  //       />
-  //       <Stack.Screen
-  //         name="Bot"
-  //         component={BotMultiline}
-  //         options={{ header: () => null }}
-  //       />
-  //     </Stack.Navigator>
-  //     {/* <PhoneCall /> */}
-  //   </NavigationContainer>
-  // );
+  if (!isLogged) {
+    console.log("user");
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="SignUp"
+            component={SignUp}
+            options={{ header: () => null }}
+          />
+          <Stack.Screen
+            name="LogIn"
+            component={LogInScreen}
+            options={{ header: () => null }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  } else {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ header: () => null }}
+          />
+          <Stack.Screen
+            name="Connecting"
+            component={Connecting}
+            options={{ header: () => null }}
+          />
+          <Stack.Screen
+            name="Chat"
+            component={Chat}
+            options={{ header: () => null }}
+          />
+          <Stack.Screen
+            name="Bot"
+            component={BotMultiline}
+            options={{ header: () => null }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
 export default App;
--- screens/Bot.js
+++ screens/Bot.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput, Keyboard, TouchableWithoutFeedback, Slider } from 'react-native';
-import * as Speech from 'expo-speech';
-import Styles from '../Styles';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import DropDownPicker from 'react-native-dropdown-picker';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  Keyboard,
+  TouchableWithoutFeedback,
+  Slider,
+} from "react-native";
+import * as Speech from "expo-speech";
+import Styles from "../Styles";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import DropDownPicker from "react-native-dropdown-picker";
 
 const Bot = (props) => {
-    const [language, setLanguage] = React.useState('en');
-    return (
-        <View>
-            <TextInput
-                {...props} // Inherit any props passed to it; e.g., multiline, numberOfLines below
-                editable
-                style={Styles.chatText}
-                clearButtonMode='always'
-            />
+  const [language, setLanguage] = React.useState("en");
+  return (
+    <View>
+      <TextInput
+        {...props} // Inherit any props passed to it; e.g., multiline, numberOfLines below
+        editable
+        style={Styles.chatText}
+        clearButtonMode="always"
+      />
 
-            {/* <DropDownPicker
+      {/* <DropDownPicker
                 items={[
                     { label: 'English', value: 'en' },
                     { label: 'Spanish', value: 'es-419' },
@@ -27,62 +36,65 @@ const Bot = (props) => {
                 onChangeItem={item => setLanguage(item)}
                 placeholder='Select a language'
             /> */}
-        </View>
-    );
-}
+    </View>
+  );
+};
 
 const BotMultiline = (props) => {
-    const [value, onChangeText] = React.useState('');
-    const [lang, setLanguage] = React.useState('en');
-    const [currentPitch, setPitch] = React.useState(1.0)
+  const [value, onChangeText] = React.useState("");
+  const [lang, setLanguage] = React.useState("en");
+  const [currentPitch, setPitch] = React.useState(1.0);
 
-    return (
+  return (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <View style={Styles.container}>
+        <TouchableOpacity
+          onPress={() =>
+            Speech.speak(
+              value,
+              { language: lang, pitch: currentPitch } /*{ pitch: 2 }*/
+            )
+          }
+        >
+          <View style={Styles.buttonBackgroundBlue}>
+            <Text style={Styles.buttonText}>Speak</Text>
+          </View>
+        </TouchableOpacity>
 
-        < TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-            <View style={Styles.container}>
-                <TouchableOpacity onPress={() => Speech.speak(value, { language: lang, pitch: currentPitch }/*{ pitch: 2 }*/)}>
-                    <View style={Styles.buttonBackgroundBlue}>
-                        <Text style={Styles.buttonText}>
-                            Speak
-                        </Text>
-                    </View>
-                </TouchableOpacity>
+        <Button onPress={() => Speech.pause()} title="Pause" />
+        <Button onPress={() => Speech.resume()} title="Resume" />
+        <Button onPress={() => Speech.stop()} title="Stop" />
 
-                <Button onPress={() => Speech.pause()} title="Pause" />
-                <Button onPress={() => Speech.resume()} title="Resume" />
-                <Button onPress={() => Speech.stop()} title="Stop" />
-
-                <View style={{ width: '90%', padding: '1%' }}>
-                    <Bot
-                        onChangeText={text => onChangeText(text)}
-                        value={value}
-                        placeholder="Type here"
-                    />
-                    <DropDownPicker
-                        items={[
-                            { label: 'English', value: 'en' },
-                            { label: 'Spanish', value: 'es-419' },
-                            { label: 'German', value: 'gsw-u-sd-chzh' }
-                        ]}
-                        defaultIndex={0}
-                        containerStyle={{ height: 40 }}
-                        onChangeItem={item => setLanguage(item.value)}
-                        placeholder='Select a language'
-                    />
-                    <Text>Pitch:</Text>
-                    <Slider
-                        maximumValue={2}
-                        minimumValue={1}
-                        step={0.01}
-                        // value={currentPitch}
-                        onValueChange={(item) => setPitch(item)}
-                        width='100%'
-                    />
-                </View>
-            </View>
-        </TouchableWithoutFeedback >
-
-    );
-}
+        <View style={{ width: "90%", padding: "1%" }}>
+          <Bot
+            onChangeText={(text) => onChangeText(text)}
+            value={value}
+            placeholder="Type here"
+          />
+          <DropDownPicker
+            items={[
+              { label: "English", value: "en" },
+              { label: "Spanish", value: "es-419" },
+              { label: "German", value: "gsw-u-sd-chzh" },
+            ]}
+            defaultIndex={0}
+            containerStyle={{ height: 40 }}
+            onChangeItem={(item) => setLanguage(item.value)}
+            placeholder="Select a language"
+          />
+          <Text>Pitch:</Text>
+          <Slider
+            maximumValue={2}
+            minimumValue={1}
+            step={0.01}
+            // value={currentPitch}
+            onValueChange={(item) => setPitch(item)}
+            width="100%"
+          />
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
 
 export default BotMultiline;
